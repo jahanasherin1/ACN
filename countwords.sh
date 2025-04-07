b317932d6e0b1332af17cf49c38e76b9feb5b435
jahanasherin1 <jahanasherin2311@gmail.com>
2025-04-07 15:28:53 +0530

    Completed

---
+++ countwords.sh
@@ -0,0 +1,29 @@
+#To count number of words and lines in a file
+if [ $# -gt 1 ]
+then
+   echo "syntax is <$0> <file name>"
+exit 1
+fi
+flag=0
+if [ $# -eq 1 ]
+then
+  term=`tty`
+  exec<$1
+  flag=1
+fi
+while read line
+do
+  nol=`expr $nol + 1`
+  set $line > null
+  nowds=`expr $nowds + $#`
+done
+echo "Number of lines = $nol"
+echo "Number of words = $nowds"
+if [ $flag -eq 1 ]
+then
+  exec < $term
+fi
+exit 0
+
+
+
