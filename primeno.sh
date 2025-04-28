21dba129f56c36cb17e4d5eda27e239b4da9cf89
jahanasherin1 <jahanasherin2311@gmail.com>
2025-04-28 15:20:06 +0530

    Completed

--- primeno.sh
+++ primeno.sh
@@ -9,14 +9,14 @@ then
 exit 1
 fi
 i=2
-while [ $i -lt `expr $1 / 2 ` ]
+while [ $i -le `expr $1 / 2 ` ]
 do
-i=`expr $i + 1`
 if [ `expr $1 % $i` -eq 0 ]
 then
 echo "$1 is not a prime number "
 exit 0
 fi
+i=`expr $i + 1`
 done
 echo " $1 is a prime number "
 fi
