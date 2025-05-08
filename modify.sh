67a6be4d98609e6f58eabc1361b173eb20321cc0
jahanasherin1 <jahanasherin2311@gmail.com>
2025-05-08 13:11:04 +0530

    Completed

---
+++ modify.sh
@@ -0,0 +1,25 @@
+#modify cp command
+
+if [ $# -ne 2 ]
+then
+   echo "Syntax is <$0><src filename><tgt filename>"
+exit 1
+fi
+if [ ! -f $1 ]
+then
+  echo "$1 is not existing or not an ordinary file"
+  exit 2
+fi
+if [ -f $2 ]
+then
+    echo " Target file exists,over write it (y/n)"
+    read ans
+if [ $ans = "n" ]
+then
+  exit 3
+fi
+fi
+cp $1 $2 
+echo "File copied" 
+
+
