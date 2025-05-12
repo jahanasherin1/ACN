17227fcb2925eeb5c996c4a7f206bfedb6571d9d
jahanasherin1 <jahanasherin2311@gmail.com>
2025-05-12 15:00:04 +0530

    Completed

---
+++ receive.sh
@@ -0,0 +1,15 @@
+#receives two filenames .if the files are identical delete one
+if [ $# -ne 2 ]
+then
+echo "Syntax is $0 <file1> <file2> "
+exit 0
+fi
+cmp -s $1 $2 
+if [ $? -eq 0 ]
+then
+echo "removing file $1"
+rm $1
+else
+echo "Files are not identical"
+fi
+exit 0
