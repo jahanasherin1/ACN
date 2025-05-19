720312edff449703016e0b08e988a8036e752468
jahanasherin1 <jahanasherin2311@gmail.com>
2025-05-19 14:21:15 +0530

    Completed

---
+++ reverse.sh
@@ -0,0 +1,16 @@
+if [ $# -ne 1 ] 
+then
+echo "Syntax is $0 <filename>"
+exit 1
+fi
+
+cp $1 file
+l=`cat file | wc -l`
+rm -f rfile
+while [ $l -ge 1 ]
+do
+tail -n 1 file >> rfile
+l=`expr $l - 1`
+head -n $l file > temp
+mv temp file
+done
