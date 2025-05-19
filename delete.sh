b07960b8960f5b470ec2492ed8dcc570a6ccbb41
jahanasherin1 <jahanasherin2311@gmail.com>
2025-05-19 15:50:51 +0530

    Completed

--- delete.sh
+++ delete.sh
@@ -1,15 +1,18 @@
-if [ $# -ne 2 ]; then
-echo "Syntaxi is $0 <file> <word to delete>"
-exit 1
+if [ $# -ne 2 ]
+then
+    echo "Syntax: $0 <file> <word to delete>"
+    exit 1
 fi
 
-sed -i "s/\b$2\b//g" "$1"
+echo "Removing lines containing '$2' from $1"
+grep -v "$2" $1 > temp
+mv temp $1
 
 if [ $? -eq 0 ]
 then
-echo " $2 deleted from $1 succesfully"
+    echo "'$2' deleted from $1 successfully"
 else
-echo "error deleting $2 from $1"
+    echo "Error deleting '$2' from $1"
 fi
 
-exit 0 
+exit 0
